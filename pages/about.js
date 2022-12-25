96a7d4382fa845b47f578a0b71145ece5d14d71d
Pavel Mezencev <mezencev24@mail.ru>
2022-12-25 17:42:16 +0300

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate in
+        ut recusandae totam magnam suscipit quasi a, odit repellat rerum iure
+        ipsa. Nulla quos sequi consectetur, veritatis aliquam repellat saepe.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate in
+        ut recusandae totam magnam suscipit quasi a, odit repellat rerum iure
+        ipsa. Nulla quos sequi consectetur, veritatis aliquam repellat saepe.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,9 @@
+const NinjasList = () => {
+  return (
+    <div>
+      <h1>NinjasList</h1>
+    </div>
+  );
+};
+
+export default NinjasList;
